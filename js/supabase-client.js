6392e2a8f420c0892c862366427f6aa4693c8e14
Shalom Karr <Shalomkarrsphone@gmail.com>
2025-12-08 00:52:59 -0500

    Update Supabase client to version 2.39.8

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.8/+esm';
 
 const SUPABASE_URL = 'https://umuiapirxdbcirvylrlw.supabase.co'; 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtdWlhcGlyeGRiY2lydnlscmx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNTYyMjgsImV4cCI6MjA2NjgzMjIyOH0.6TYu5Adm-rjrptcC_2IpJUT7UgDeK48m6E-Omz8PDfE'; 
